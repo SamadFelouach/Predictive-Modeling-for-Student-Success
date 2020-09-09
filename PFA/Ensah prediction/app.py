6fe13f1e629ebe06a11089684a240a35e2e794ad
ABDESSAMAD FELOUACH <abdessamadfelouach@gmail.com>
2020-09-09 13:13:26 +0100

    unzip

---
+++ PFA/Ensah prediction/app.py
@@ -0,0 +1,72 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/prediction')
+def predi():
+    return render_template('Predict.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('Predict.html', prediction_text='Student Prediction should be $ {}'.format(output))
+
+
+@app.route('/chart')
+def chartes(chartID = 'chart_ID', chart_type = 'bar', chart_height = 350):
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = [{"name": 'Café', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+	title = {"text": 'My Title'}
+	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+	yAxis = {"title": {"text": 'yAxis Label'}}
+    
+    
+	return render_template('chart.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+def chartes2(chartID = 'chart2', chart_type = 'bar', chart_height = 350):
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = [{"name": 'non café', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+	title = {"text": 'My Title'}
+	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+	yAxis = {"title": {"text": 'yAxis Label'}}
+    
+    
+	return render_template('chart.html', chartID=chart2, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
